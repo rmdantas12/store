1cb350ae0c897793216e1f38844057b8bb31a34e
Rodrigo Dantas <contato@rmdantas@gmail.com>
2026-03-31 20:42:18 -0300

    chore: Controller dos serviços REST de clientes
    Signed-off-by: Rodrigo Dantas <contato@rmdantas@gmail.com>

---
+++ src/main/java/com/perinity/store/infrastructure/web/customer/CustomerController.java
@@ -0,0 +1,72 @@
+package com.perinity.store.infrastructure.web.customer;
+
+import com.perinity.store.domain.ports.incoming.CustomerUseCase;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class CustomerController implements CustomerAPI {
+
+  private final CustomerUseCase customerService;
+
+  private final CustomerWebMapper customerWebMapper;
+
+  private final UriInfo uriInfo;
+
+  @Override
+  public Response create(final CustomerRequest request) {
+    final var customer = customerWebMapper.toDomain(request);
+    final var created = customerService.create(customer);
+
+    final var location = uriInfo.getAbsolutePathBuilder()
+        .path(created.getCode().toString())
+        .build();
+
+    final var body = customerWebMapper.toResponse(created);
+
+    return Response.created(location)
+        .entity(body)
+        .build();
+  }
+
+  @Override
+  public Response update(final UUID code, final CustomerUpdateRequest request) {
+    final var customer = customerWebMapper.toDomain(request);
+    final var updated = customerService.update(code, customer);
+    final var body = customerWebMapper.toResponse(updated);
+
+    return Response.ok(body)
+        .build();
+  }
+
+  @Override
+  public Response delete(final UUID code) {
+    customerService.delete(code);
+
+    return Response.noContent()
+        .build();
+  }
+
+  @Override
+  public Response findByCode(final UUID code) {
+    final var customer = customerService.findByCode(code);
+    final var body = customerWebMapper.toResponse(customer);
+
+    return Response.ok(body)
+        .build();
+  }
+
+  @Override
+  public Response findAll() {
+    final var body = customerService.findAll().stream()
+        .map(customerWebMapper::toResponse)
+        .toList();
+
+    return Response.ok(body)
+        .build();
+  }
+
+}
