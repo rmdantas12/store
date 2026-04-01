c22cf8d338730c9bf9b1b4efcaa17cb6cc636d66
Rodrigo Dantas <contato@rmdantas@gmail.com>
2026-04-01 13:20:55 -0300

    chore: Resolvendo warn do mapstruct
    Signed-off-by: Rodrigo Dantas <contato@rmdantas@gmail.com>

--- src/main/java/com/perinity/store/infrastructure/persistence/product/ProductPersistenceMapper.java
+++ src/main/java/com/perinity/store/infrastructure/persistence/product/ProductPersistenceMapper.java
@@ -2,10 +2,12 @@ package com.perinity.store.infrastructure.persistence.product;
 
 import com.perinity.store.domain.model.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "cdi")
 public interface ProductPersistenceMapper {
 
+  @Mapping(target = "updatedAt", ignore = true)
   ProductEntity toEntity(Product product);
 
   Product toDomain(ProductEntity entity);
