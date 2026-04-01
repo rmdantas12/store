415db177248912b8463c1fe77f4f0ab43219c798
Rodrigo Dantas <contato@rmdantas@gmail.com>
2026-04-01 00:25:22 -0300

    chore: Configurando handler para as exceções
    Signed-off-by: Rodrigo Dantas <contato@rmdantas@gmail.com>

---
+++ src/main/java/com/perinity/store/infrastructure/web/exception/ProductNotFoundExceptionMapper.java
@@ -0,0 +1,18 @@
+package com.perinity.store.infrastructure.web.exception;
+
+import com.perinity.store.domain.exception.ProductNotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.Provider;
+
+/**
+ * Mapeia {@link ProductNotFoundException} para HTTP 404.
+ */
+@Provider
+public class ProductNotFoundExceptionMapper implements BaseExceptionMapper<ProductNotFoundException> {
+
+  @Override
+  public Response.Status getStatus() {
+    return Response.Status.NOT_FOUND;
+  }
+}
+
