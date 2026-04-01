75248219578e14b57d78b0ee9c0c2716bb9c2867
Rodrigo Dantas <contato@rmdantas@gmail.com>
2026-03-31 22:02:22 -0300

    fix: Ajustando anotações da controller de clientes
    Signed-off-by: Rodrigo Dantas <contato@rmdantas@gmail.com>

--- src/main/java/com/perinity/store/infrastructure/web/customer/CustomerController.java
+++ src/main/java/com/perinity/store/infrastructure/web/customer/CustomerController.java
@@ -1,6 +1,12 @@
 package com.perinity.store.infrastructure.web.customer;
 
 import com.perinity.store.domain.ports.incoming.CustomerUseCase;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import lombok.RequiredArgsConstructor;
@@ -8,13 +14,18 @@ import lombok.RequiredArgsConstructor;
 import java.util.UUID;
 
 @RequiredArgsConstructor
+@Path("/api/customers")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@ApplicationScoped
 public class CustomerController implements CustomerAPI {
 
   private final CustomerUseCase customerService;
 
   private final CustomerWebMapper customerWebMapper;
 
-  private final UriInfo uriInfo;
+  @Context
+  private UriInfo uriInfo;
 
   @Override
   public Response create(final CustomerRequest request) {
